48900d712b594cbbe29163a6c088ecaedba95b5c
edisonmbli <edisonmbli@gmail.com>
2019-06-20 00:38:01 +0800

    core-3

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Req, Query, Headers, Param, Post, Body, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers, Param, Post, Body, HttpException, HttpStatus, UseFilters, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, SetMetadata } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
 import { DemoService } from './providers/demo/demo.service';
 import { DemoFilter } from '../../core/filters/demo.filter';
+import { DemoAuthGuard } from '../../core/guards/demo-auth.guard';
+import { Roles } from '../../core/decorators/roles.decorator';
 
 
 @Controller('posts')
+//@UseGuards(DemoAuthGuard)
 export class PostsController {
     constructor(private readonly demoService: DemoService) {
         this.demoService = demoService;
@@ -16,16 +19,21 @@ export class PostsController {
     }
 
     @Get(':id')
-    show(@Param() params) {
+    show(@Param('id', ParseIntPipe) id) {
+        console.log('id:', typeof id);
+
         return {
-            title: `Post ${params.id}`
+            title: `Post ${id}`
         }
     }
 
     @Post()
-    @UseFilters(DemoFilter)
+    //@UseFilters(DemoFilter)
+    @UsePipes(ValidationPipe)
+    //@SetMetadata('roles', ['member'])
+    @Roles('member')
     store(@Body() post: CreatePostDto) {
-        throw new HttpException('Permission Denied', HttpStatus.FORBIDDEN);
-        //this.demoService.create(post);
+        //throw new HttpException('Permission Denied', HttpStatus.FORBIDDEN);
+        this.demoService.create(post);
     }
 }
