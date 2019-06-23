67ca3da759518ec995bf450fc19bfc1731888166
edisonmbli <edisonmbli@gmail.com>
2019-06-23 10:59:05 +0800

    #6 data-manipulation

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import { async } from 'rxjs/internal/scheduler/async';
+import { PostService } from './post.service';
+import { PostDto } from './post.dto';
 
 @Controller('posts')
-export class PostController {}
+export class PostController {
+    constructor(
+        private readonly postService: PostService
+    ) {}
+
+    @Post()
+    async store(@Body() data: PostDto) {
+        return await this.postService.store(data);
+    }
+
+    @Get()
+    async index() {
+        return await this.postService.index();
+    }
+
+    @Get(':id')
+    async show(@Param('id') id: string) {
+        return await this.postService.show(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() data: Partial<PostDto>) {
+        return await this.postService.update(id, data);
+    }
+
+    @Delete(':id')
+    async destroy(@Param('id') id: string) {
+        return await this.postService.destroy(id);
+    }
+}
