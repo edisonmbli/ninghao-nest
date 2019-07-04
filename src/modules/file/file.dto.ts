1b96d087d219a74b3c73d16728c5402cd08b5957
edisonmbli <edisonmbli@gmail.com>
2019-07-05 00:32:19 +0800

    #15 files

---
+++ src/modules/file/file.dto.ts
@@ -0,0 +1,10 @@
+export class FileDto {
+    readonly fieldname: string;
+    readonly originalname: string;
+    readonly enconding: string;
+    readonly mimetype: string;
+    readonly destination: string;
+    readonly filename: string;
+    readonly path: string;
+    readonly size: number;
+}
---
+++ src/modules/file/file.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { FileDto } from './file.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { File } from './file.entity';
+
+@Injectable()
+export class FileService {
+    constructor(
+        @InjectRepository(File)
+        private readonly fileRepository: Repository<File>
+    ) {}
+    
+    async store(data: FileDto) {
+        return await this.fileRepository.save(data);
+    }
+
+    async show(id: number) {
+        return await this.fileRepository.findOne(id);
+    }
+}
